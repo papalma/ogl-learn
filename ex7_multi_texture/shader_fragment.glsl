7a8b2f46f449efe0c63d70e339a9537614e60401
Patricio Palma <patriciop@gmail.com>
2019-07-07 20:32:48 -0400

    No coloring on multi texture example.

--- ex7_multi_texture/shader_fragment.glsl
+++ ex7_multi_texture/shader_fragment.glsl
@@ -8,8 +8,7 @@ out vec4 FragColor;
 uniform sampler2D texture1;
 uniform sampler2D texture2;
 
-
 void main()
 {
-    FragColor = mix(texture(texture1, texCoord), texture(texture2, texCoord), 0.3) * vec4(ourColor,1.0);
+    FragColor = mix(texture(texture1, texCoord), texture(texture2, texCoord), 0.3);
 }
